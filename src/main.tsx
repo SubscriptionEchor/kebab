7532a2960355104b111c27db36aa8bf65388cc9a
Phanikondru <phanikondru@gmail.com>
2025-04-25 14:36:22 +0530

    Latest code

--- src/main.tsx
+++ src/main.tsx
@@ -8,12 +8,12 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-      <I18nextProvider i18n={i18n}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <I18nextProvider i18n={i18n}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </I18nextProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
 
 
