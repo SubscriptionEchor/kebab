1ecab53f3b6fd6cdbb6ab46d3860a3b92415d826
Phanikondru <phanikondru@gmail.com>
2025-04-25 14:47:11 +0530

    Latest code

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import { I18nextProvider } from 'react-i18next';
-import i18n from './i18.ts';
-import App from './App.tsx';
-import './index.css';
+import i18n from './i18.ts'
+import App from './App.tsx'
+import './index.css'
 
-// Ensure the root element exists before rendering
-const rootElement = document.getElementById('root');
-if (!rootElement) throw new Error('Root element not found');
-
-ReactDOM.createRoot(rootElement).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    {/* <I18nextProvider i18n={i18n}> */}
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    {/* </I18nextProvider> */}
-  </React.StrictMode>
-);
+      <I18nextProvider i18n={i18n}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    </I18nextProvider>
+  </React.StrictMode>,
+)
