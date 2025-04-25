6bbba4519bb2ffd53aa539a11bc91094f1964007
Phanikondru <phanikondru@gmail.com>
2025-04-25 14:43:58 +0530

    Latest code

--- src/i18.ts
+++ src/i18.ts
@@ -5,26 +5,25 @@ import translationtr from './locales/tr.json';
 import translationde from './locales/de.json';
 import translationhi from './locales/hi.json';
 
-
 const resources = {
   en: { translation: translationEN },
   tr: { translation: translationtr },
   de: { translation: translationde },
   hi: { translation: translationhi },
-  //ta: { translation: translationTA }
 };
+
 i18n
   .use(initReactI18next)
-  .init({ 
-    resources,
-    lng: 'en', // Default language
-    fallbackLng: 'en',
-    interpolation: { escapeValue: false }
-  });
+  .init(
+    { 
+      resources,
+      lng: 'en',
+      fallbackLng: 'en',
+      interpolation: { escapeValue: false }
+    },
+    (err) => {
+      if (err) console.error('i18next initialization failed:', err);
+    }
+  );
+
 export default i18n;
-
-
-
-
-
-
